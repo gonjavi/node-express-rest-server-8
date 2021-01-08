e55e7f68dbe43ca86365d77d0f05682776bbe70a
manci <javiercaliescali@gmail.com>
2021-01-08 18:39:12 -0500

    al borrar usuario solo cambiar estado a false y solo mostrar usuarios en estado true

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -11,7 +11,7 @@ app.get('/usuario', function (req, res) {
   let limite = req.query.limite || 5;
   limite = Number(limite);
 
-  Usuario.find({}, 'nombre email role estado google img')  // para filtrar
+  Usuario.find({estado: true}, 'nombre email role estado google img')  // para filtrar
     .skip(desde)
     .limit(limite)
     . exec((err, usuarios) => {
@@ -22,7 +22,7 @@ app.get('/usuario', function (req, res) {
         });
       }
 
-      Usuario.count({}, (err, conteo) => {
+      Usuario.count({estado: true}, (err, conteo) => {
         res.json({
           ok: true,
           usuarios,
@@ -78,7 +78,11 @@ app.put('/usuario/:id', function (req, res) {
 app.delete('/usuario/:id', function (req, res) {
   let id = req.params.id;
 
-  Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+  let cambiaEstado = {
+    estado: false
+  }
+  // Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+    Usuario.findByIdAndUpdate(id, cambiaEstado, {new: true},  (err, usuarioBorrado) => {
     if (err) {
       return res.status(400).json({
         ok: false,
