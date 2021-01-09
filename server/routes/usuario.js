fb0644a953950737ef3f30a566c658c7179fb8b2
manci <javiercaliescali@gmail.com>
2021-01-09 18:33:22 -0500

    125 agregar verificaciond e rol administrador para borrar, actualizar y crear usuario

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -3,15 +3,10 @@ const bcrypt = require('bcrypt');
 const _ = require('underscore');
 const app = express();
 const Usuario = require('../models/usuario');
-const { verificaToken } = require('../middlewares/autenticacion');
+const { verificaToken, verificaAdmin_Role } = require('../middlewares/autenticacion');
 
 app.get('/usuario', verificaToken, (req, res) => {
 
-  /* return res.json({
-    usuario: req.usuario,
-    nombre: req.usuario.nombre,
-    email: req.usuario.email,
-  }) */
   let desde = req.query.desde || 0;
   desde = Number(desde);
 
@@ -39,7 +34,7 @@ app.get('/usuario', verificaToken, (req, res) => {
     });
 });
 
-app.post('/usuario', function (req, res) {
+app.post('/usuario', [verificaToken, verificaAdmin_Role ],function (req, res) {
   let body = req.body;
 
   let usuario = new Usuario({
@@ -64,11 +59,11 @@ app.post('/usuario', function (req, res) {
   });  
 });
 
-app.put('/usuario/:id', verificaToken, (req, res) => {
+app.put('/usuario/:id', [verificaToken, verificaAdmin_Role ], (req, res) => {
   let id = req.params.id;
   let body = _.pick(req.body, ['nombre','email','img','role','estado']); //no actualiza contraseña ni google
 
-  Usuario.findByIdAndUpdate(id, body, {new: true, runValidators: true }, (err, usuarioDB) => {
+  Usuario.findByIdAndUpdate(id, body, {new: true, runValidators: true,  useFindAndModify: false }, (err, usuarioDB) => {
     if (err) {
       return res.status(400).json({
         ok: false,
@@ -82,14 +77,14 @@ app.put('/usuario/:id', verificaToken, (req, res) => {
   }); 
 });
 
-app.delete('/usuario/:id', verificaToken, (req, res) => {
+app.delete('/usuario/:id', [verificaToken, verificaAdmin_Role ], (req, res) => {
   let id = req.params.id;
 
   let cambiaEstado = {
     estado: false
   }
   // Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
-    Usuario.findByIdAndUpdate(id, cambiaEstado, {new: true},  (err, usuarioBorrado) => {
+    Usuario.findByIdAndUpdate(id, cambiaEstado, {new: true,  useFindAndModify: false},  (err, usuarioBorrado) => {
     if (err) {
       return res.status(400).json({
         ok: false,
