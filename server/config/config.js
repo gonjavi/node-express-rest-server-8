a66e3dd7a027dfa90a89a42c9f0499f033c30fae
manci <javiercaliescali@gmail.com>
2021-01-08 21:51:13 -0500

    borrar credenciales mongo cloud y agregarlas en MONRO_URI en heroku 2

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
   urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-  urlDB = process.env.NODE_ENV.MONGO_URI; //MONGO_URI es creada en heroku y el valor es aisgnado
+  urlDB = process.env.MONGO_URI; //MONGO_URI es creada en heroku y el valor es aisgnado
 }
 
 process.env.URLDB = urlDB;
