3b13aa735bd23d7b82245f84a7f0fabdc7a54a2f
manci <javiercaliescali@gmail.com>
2021-01-09 16:18:37 -0500

    g124 verificar token y obtener informacion del payload en cualquier servicio

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -5,7 +5,13 @@ const app = express();
 const Usuario = require('../models/usuario');
 const { verificaToken } = require('../middlewares/autenticacion');
 
-app.get('/usuario', verificaToken, function (req, res) {
+app.get('/usuario', verificaToken, (req, res) => {
+
+  /* return res.json({
+    usuario: req.usuario,
+    nombre: req.usuario.nombre,
+    email: req.usuario.email,
+  }) */
   let desde = req.query.desde || 0;
   desde = Number(desde);
 
@@ -58,7 +64,7 @@ app.post('/usuario', function (req, res) {
   });  
 });
 
-app.put('/usuario/:id', function (req, res) {
+app.put('/usuario/:id', verificaToken, (req, res) => {
   let id = req.params.id;
   let body = _.pick(req.body, ['nombre','email','img','role','estado']); //no actualiza contraseña ni google
 
@@ -76,7 +82,7 @@ app.put('/usuario/:id', function (req, res) {
   }); 
 });
 
-app.delete('/usuario/:id', function (req, res) {
+app.delete('/usuario/:id', verificaToken, (req, res) => {
   let id = req.params.id;
 
   let cambiaEstado = {
