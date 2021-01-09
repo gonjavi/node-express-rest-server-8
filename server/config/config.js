70eb06bbaa0103f1e1f28872cccc8b93fc3b6171
manci <javiercaliescali@gmail.com>
2021-01-08 20:39:41 -0500

    adaptar config para conexion a mongo atlas cloud or localhosta ambiente

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,17 @@
 
 // puerto
 process.env.PORT = process.env.PORT || 3000;
+
+// entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// Base de datos
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+  urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+  urlDB = 'mongodb+srv://javi:<3MiYsEFH3AMqH6I0>@cluster0.wkr6a.mongodb.net/test';
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -12,8 +12,8 @@ app.use(bodyParser.json());
 //importar rutas de usuario
 app.use(require('./routes/usuario'));
 
-mongoose.connect('mongodb://localhost:27017/cafe', 
-  { useNewUrlParser: true, useCreateIndex: true }, 
+mongoose.connect(process.env.URLDB, 
+  { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }, 
   (err, res) => {
   if (err) throw err;
     console.log('Base de datos en linea'); 
