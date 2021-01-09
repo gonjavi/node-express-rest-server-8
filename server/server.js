e99e07e83679b0c0f93944c067ac1835b03d5cec
manci <javiercaliescali@gmail.com>
2021-01-08 19:53:56 -0500

    actualizar conexion mongoose para evitar error eprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead

--- server/server.js
+++ server/server.js
@@ -12,11 +12,11 @@ app.use(bodyParser.json());
 //importar rutas de usuario
 app.use(require('./routes/usuario'));
 
-mongoose.connect('mongodb://localhost:27017/cafe', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true
+mongoose.connect('mongodb://localhost:27017/cafe', 
+  { useNewUrlParser: true, useCreateIndex: true }, 
+  (err, res) => {
+  if (err) throw err;
+    console.log('Base de datos en linea'); 
 });
 
 app.listen(process.env.PORT, () => console.log("Escuchando puerto", process.env.PORT));
