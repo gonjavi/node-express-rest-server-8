518c8ce6c310b5a59e3e72efba16b4e97916ffc7
manci <javiercaliescali@gmail.com>
2021-01-09 18:59:45 -0500

    corregir DeprecationWarning: Mongoose findOneAndUpdate() and findOneAndDelete() without the useFindAndModify option set to false are deprecated. See: https://mongoosejs.com/docs/deprecations.html#findandmodify

---
+++ server/middlewares/autenticacion.js
@@ -0,0 +1,46 @@
+const jwt = require('jsonwebtoken');
+
+// Verifficar token
+let verificaToken = (req, res, next) => {
+  let token = req.get('token');
+
+  jwt.verify(token, process.env.SEED, (err, decoded) => {
+    // sino hay error la info es correcta - el decoded contiene la info del usuario
+    if(err) {
+      return res.status(401).json({
+        ok: false,
+        err: {
+          message: 'Token no valido'
+        }
+      });
+    }    
+
+    req.usuario = decoded.usuario;
+    // ejecuto el next para que continue el programa
+    next();
+  })
+ 
+};
+
+// verifica admin rol
+let verificaAdmin_Role = (req, res, next) => {
+  let usuario = req.usuario;
+
+  if (usuario.role === 'ADMIN_ROLE') {
+    next();
+  } else {
+    res.json({
+      ok: true,
+      err: {
+        message: 'El usuario no es administrador'
+      }
+    });
+  }
+  
+};
+
+
+module.exports = {
+  verificaToken,
+  verificaAdmin_Role,
+}
