482c6776cc545ad7ae890c7db8d44f3de831fa6a
manci <javiercaliescali@gmail.com>
2021-01-09 15:03:56 -0500

    generar json web token

--- server/config/config.js
+++ server/config/config.js
@@ -5,6 +5,14 @@ process.env.PORT = process.env.PORT || 3000;
 // entorno
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
+// Vencimiento del token
+// 60 segundos  60 minutos  24 horas 30 dias
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+
+// seed de autenticacion
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
+
 // Base de datos
 let urlDB;
 
