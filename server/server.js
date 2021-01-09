ef45d447b826f9e526f020d53244a47664197c91
manci <javiercaliescali@gmail.com>
2021-01-09 14:29:24 -0500

    creacion de login

--- server/server.js
+++ server/server.js
@@ -2,15 +2,14 @@ require('./config/config');
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
-
 const bodyParser = require('body-parser');
  // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false })); 
 // parse application/json
 app.use(bodyParser.json());
+// Configuración global de rutas
+app.use(require('./routes/index'));
 
-//importar rutas de usuario
-app.use(require('./routes/usuario'));
 
 mongoose.connect(process.env.URLDB, 
   { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }, 
