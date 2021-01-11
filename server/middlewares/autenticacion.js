218fcf8cd56196a518eb4091b7d234894b494a58
manci <javiercaliescali@gmail.com>
2021-01-10 22:05:11 -0500

    agregar middleware agregar token por url

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -18,8 +18,7 @@ let verificaToken = (req, res, next) => {
     req.usuario = decoded.usuario;
     // ejecuto el next para que continue el programa
     next();
-  })
- 
+  }); 
 };
 
 // verifica admin rol
@@ -39,8 +38,29 @@ let verificaAdmin_Role = (req, res, next) => {
   
 };
 
+// verifica token en img
+let verificaTokenImg = (req, res, next) => {
+  let token = req.query.token;
+
+  jwt.verify(token, process.env.SEED, (err, decoded) => {
+    // sino hay error la info es correcta - el decoded contiene la info del usuario
+    if(err) {
+      return res.status(401).json({
+        ok: false,
+        err: {
+          message: 'Token no valido'
+        }
+      });
+    }    
+
+    req.usuario = decoded.usuario;
+    // ejecuto el next para que continue el programa
+    next();
+  });
+}
 
 module.exports = {
   verificaToken,
   verificaAdmin_Role,
+  verificaTokenImg,
 }
